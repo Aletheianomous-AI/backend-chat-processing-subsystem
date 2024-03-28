2821ae9ef6eb1926b1d9d1eaf634cbc20ee8278e
David Chavarro <dchavar2@students.kennesaw.edu@alethianomous-ai-backend-dev.xj3w0pbtkl3efbadsgqflnv5mh.bx.internal.cloudapp.net>
2024-03-28 16:38:05 +0000

    Tested and fixed bugs for chat_data_module.py

--- data_management_subsystem/chat_data_module.py
+++ data_management_subsystem/chat_data_module.py
@@ -1,14 +1,18 @@
+from pytz import timezone
+
 import pyodbc
 import pandas as pd
 
 
 class ChatData():
     def __init__(self, userId):
-    
-        self.conn = pyodbc.connect('DRIVER= {ODBC Driver 18 for SQL Server}; \
-                        SERVER=.alethianomousserver.database.windows.net; \
-                        DATABASE=alethianomousserver; \
-                        Trusted_Connection=yes')
+        server = "alethianomousserver.database.windows.net"
+        db_name = "Alethianomous AI"
+        uname = "sp15"
+        passwrd = "200322927eE!"
+
+        con_str = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={db_name};UID={uname};PWD={passwrd}'
+        self.conn = pyodbc.connect(con_str)
     
         self.userId = userId
         # if UserAccountManagement.user_exists(userId):
@@ -17,7 +21,7 @@ class ChatData():
             # raise NonExistentUserException(userId)
     
     
-    def log_chat(chat_data, timestamp, is_from_bot):
+    def log_chat(self, chat_data, timestamp, is_from_bot):
         """Uploads the chat data to the SQL database.
     
             PARAMETERS:
@@ -26,10 +30,14 @@ class ChatData():
             is_from_bot - If true, the chat is from the AI chatbot. 
             Otherwise, the chat is from the AI.
         """
-        #chat_id = get_new_chat_id()
-    
-        
-        
+       
+        chat_data = chat_data.replace("'", "''")
+        print("Parsed chat data: " + chat_data)
+
+        timestamp = timestamp.astimezone(timezone('America/New_York'))
+        timestamp = timestamp.strftime("%Y%m%d %I:%M:%S %p")
+        print(timestamp)
+
         # CREATE SQL QUERY THAT UPLOADS CHAT DATA.
         upload_query = ("""
         BEGIN TRANSACTION
@@ -37,42 +45,43 @@ class ChatData():
     		SELECT @ChatID = (MAX(Chat_ID) + 1)
             FROM dbo.Chat_History;
             INSERT INTO dbo.Chat_History (Chat_ID, Chat_Content, Belongs_To_Bot, Time_Of_Output)
-            VALUES (@ChatID, 'It is going well. Thanks for asking!', 1, '20240327 1:36:00 PM');
+            VALUES (@ChatID, '""" + chat_data + """', 1, '""" + timestamp+ """');
             INSERT INTO dbo.Chat_User (Chat_ID, UserID) VALUES (@ChatID, 1);
         COMMIT
+
         """
         )
     
         cursor = self.conn.cursor()
         cursor.execute(upload_query)
-        chat_id = cursor.fetchone()
         del cursor
+
+        cursor = self.conn.cursor()
+        chat_id_qry = ("""
+            SELECT MAX(Chat_ID) FROM dbo.Chat_History;
+        """
+        )
+        cursor.execute(chat_id_qry)
+        chat_id = cursor.fetchall()
         return chat_id
     
-    def return_chat_history(user_id):
-        # USE SQL QUERY THAT RETURNS CHAT HISTORY OF FOR USER ID
-        fetching_query = ("""
-            WITH chat_id_table AS (
-                SELECT chat_id
-                FROM public.chat_user
-                WHERE user_id = """ + str(user_id) + """
-                ORDER BY chat_id DESC
-            ),
-            chat_history_table AS (
-                SELECT *
-                FROM public.chat_history
-                ORDER BY chat_id DESC
-            )
+    def return_chat_history(self):
+
+        fetching_query = """
             SELECT *
-            FROM chat_history_table
-            INNER JOIN chat_id_table
-            ON chat_id_table.chat_id = chat_history_table.chat_id;
-        """)
+            FROM dbo.Chat_History AS ch
+            INNER JOIN dbo.Chat_User AS cu
+                ON (cu.Chat_ID = ch.Chat_ID)
+            WHERE cu.UserId = """ + str(self.userId) + """
+            ORDER BY cu.Chat_ID DESC
+        """
+        
+
         cursor = self.conn.cursor()
-        cursor.execute(upload_query)
+        cursor.execute(fetching_query)
         row = cursor.fetchone()
-        chat_hist = [row.copy()]
+        chat_hist = [row]
         while row is not None:
             row = cursor.fetchone()
-            chat_hist.append(row.copy())
-        print(chat_hist)
+            chat_hist.append(row)
+        return chat_hist
