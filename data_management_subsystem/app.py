86a2db96ecf25cad683da84b97105ba062584d1f
David Chavarro <david.e.chavarro@outlook.com>
2024-03-28 14:03:26 -0400

    Update app.py

--- data_management_subsystem/app.py
+++ data_management_subsystem/app.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 @app.route('/post_chat', methods=['POST'])
 def post_chat():
     if request.method == "POST":
+        print(request.get_json(silent=True))
         return json.dumps({'success': True}), 201
     else:
         return json.dumps({'success': False}), 400
 
-@app.route('/get_chat', methods=['GET'])
-def get_chat():
+@app.route('/get_chat/<user_id>', methods=['GET'])
+def get_chat(user_id):
     if request.method == "GET":
-        return json.dumps({'success': True, 'content': "Hello world"}), 201
+        return json.dumps({'success': True, 'content': "Hello world", 'user_id': user_id}), 201
     else:
         return json.dumps({'success': False, 'content': "You didnt ask to get it!"}), 400
 
