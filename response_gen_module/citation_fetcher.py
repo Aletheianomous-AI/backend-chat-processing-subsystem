1c5ea529c97396742ba34f0d66e3f10c23bbd75f
David Chavarro <david.e.chavarro@outlook.com>
2024-04-19 17:23:41 -0400

    Update citation_fetcher.py

--- response_gen_module/citation_fetcher.py
+++ response_gen_module/citation_fetcher.py
@@ -2,12 +2,23 @@ from duckduckgo_search import DDGS as searcher
 from duckduckgo_search.exceptions import DuckDuckGoSearchException as ddgse
 
 class Citation_Fetcher():
+
+    def parse_results(raw_results):
+        results_output = ""
+        i = 1
+        for item in raw_results:
+            results_output += "["  + str(i) + "] "
+            results_output += item['href'] + " : "
+            results_output += item['body'] + "\n"
+            i+=1
+        return results_output
     
     def search_online(query):
         raw_results = None
         try:
             search_instance = searcher()
             raw_results = search_instance.text(query, max_results=3)
+            parsed_results = self.parse_results(raw_results)
         except ddgse:
             raise ddgse()
-        return raw_results
+        return raw_results, parsed_results
